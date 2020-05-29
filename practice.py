e3e0bad8d7b2d6727a3b37012ff08944231dd2a4
Janki Dhanani <jankidhanani@jankis-air.myfiosgateway.com>
2020-05-28 23:48:49 -0400

    Practice file update with function practice

--- practice.py
+++ practice.py
@@ -1,40 +1,45 @@
-# x=int(input("Enter hours"))
-# if x<=40:
-#     x=x*12
-#     print(x)
-# elif x>40:
-#     x=(40*12)+((x-40)*(12*1.5))
-#     print (x)
+# # x=int(input("Enter hours"))
+# # if x<=40:
+# #     x=x*12
+# #     print(x)
+# # elif x>40:
+# #     x=(40*12)+((x-40)*(12*1.5))
+# #     print (x)
 
-# student=int(input("Enter your credit"))
-# if student>=120:
-#     print("Congradulations! You have enought credit to graduate")
-# elif student<120:
-#     print("Sorry! You don't have enought credit to graduate now")
+# # student=int(input("Enter your credit"))
+# # if student>=120:
+# #     print("Congradulations! You have enought credit to graduate")
+# # elif student<120:
+# #     print("Sorry! You don't have enought credit to graduate now")
 
-# for i in range(1,12):
-#     if i%2==0:
-#         print(i)
-#         break
-# else:
-#     print("not printed")
+# # for i in range(1,12):
+# #     if i%2==0:
+# #         print(i)
+# #         break
+# # else:
+# #     print("not printed")
 
-# def name12(fname):
-#     print( + "Dhanani")
+# # def name12(fname):
+# #     print( + "Dhanani")
 
-# name12("Janki ")
-# name12("Sapana ")
-# name12("Dhaval ")
+# # name12("Janki ")
+# # name12("Sapana ")
+# # name12("Dhaval ")
 
-def add():
-    a,b = 2,3
-    print("result is ", a+b)
+# def add():
+#     a,b = 2,3
+#     print("result is ", a+b)
 
-add()
+# add()
 
-def sub():
-    a,b = 2,3
-    res = a-b
-    print (res)
+# def sub():
+#     a,b = 2,3
+#     res = a-b
+#     print (res)
 
-sub()
+# sub()
+
+def name():
+    print ("Janki")
+
+print (name())
