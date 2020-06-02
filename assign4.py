ff2b55c7592851081a07fe4d9652db77a69a75e1
Janki Dhanani <jankidhanani@jankis-air.myfiosgateway.com>
2020-06-02 12:56:51 -0400

    part of assignment4 is uploaded

---
+++ assign4.py
@@ -0,0 +1,67 @@
+# print("Ans. 1")
+
+# w="1234abcd"
+# print(w[::-1])
+
+# print("OR")
+
+# def reverse(stng):
+#     new =''
+#     length=len(stng)
+
+#     while length > 0:
+#         new+=stng[length-1]
+#         length=length-1
+#     return new
+
+# print(reverse("Consultadd"))
+
+
+# print("Ans. 2")
+# q=input("Enter any value")
+# count=0
+# count1=0
+
+# for i in q:
+#     if i.islower():
+#         count+=1
+#     if i.isupper():
+#         count1+=1
+
+# print("Total lowercases are = ", count)
+# print("Total uppercases are = ", count1)
+
+# print("Ans. 3")
+# x=[12,23,12,45,45,67,83,"we","we","w2e"]
+# q=set(x)
+# print(q)
+
+# print("Ans. 4")
+# print("Enter a hyphen separated sequence of words:")
+# lst=[n for n in input().split('-')]  
+# lst.sort()
+# print("Sorted:")
+# print('-'.join(lst))
+
+# print("Ans. 5")
+# x=input("Enter a sentence").upper()
+# print(x)
+
+# print("Ans. 8")
+
+# def ans():
+#     x=[i**2 for i in range(1,21)]
+#     print (x)
+
+# ans()
+
+# print ("OR")
+
+# def is_even():
+#     list1=[]
+#     for i in range(1,20):
+#             print(i**2)
+#     return list1
+
+# print (is_even())
+
--- practice.py
+++ practice.py
@@ -1,40 +1,90 @@
-# x=int(input("Enter hours"))
-# if x<=40:
-#     x=x*12
-#     print(x)
-# elif x>40:
-#     x=(40*12)+((x-40)*(12*1.5))
-#     print (x)
+# # # # x=int(input("Enter hours"))
+# # # # if x<=40:
+# # # #     x=x*12
+# # # #     print(x)
+# # # # elif x>40:
+# # # #     x=(40*12)+((x-40)*(12*1.5))
+# # # #     print (x)
 
-# student=int(input("Enter your credit"))
-# if student>=120:
-#     print("Congradulations! You have enought credit to graduate")
-# elif student<120:
-#     print("Sorry! You don't have enought credit to graduate now")
+# # # # student=int(input("Enter your credit"))
+# # # # if student>=120:
+# # # #     print("Congradulations! You have enought credit to graduate")
+# # # # elif student<120:
+# # # #     print("Sorry! You don't have enought credit to graduate now")
 
-# for i in range(1,12):
-#     if i%2==0:
-#         print(i)
-#         break
-# else:
-#     print("not printed")
+# # # # for i in range(1,12):
+# # # #     if i%2==0:
+# # # #         print(i)
+# # # #         break
+# # # # else:
+# # # #     print("not printed")
 
-# def name12(fname):
-#     print( + "Dhanani")
+# # # # def name12(fname):
+# # # #     print( + "Dhanani")
 
-# name12("Janki ")
-# name12("Sapana ")
-# name12("Dhaval ")
+# # # # name12("Janki ")
+# # # # name12("Sapana ")
+# # # # name12("Dhaval ")
 
-def add():
-    a,b = 2,3
-    print("result is ", a+b)
+# # # def add():
+# # #     a,b = 2,3
+# # #     print("result is ", a+b)
 
-add()
+# # # add()
 
-def sub():
-    a,b = 2,3
-    res = a-b
-    print (res)
+# # # def sub():
+# # #     a,b = 2,3
+# # #     res = a-b
+# # #     print (res)
 
-sub()
+# # # sub()
+
+# # f=[1,5,6,10,7,16]
+# # for i in f:
+# #     if i%2==0:
+# #         print(i)
+# # else:
+# #     print("there is no odd number")
+
+# # items = [25,26,27,28]
+# # square=list(map(lambda x:x**2, items))
+# # print(square)
+
+# # from functools import reduce
+# # def minus():
+# #     er=reduce(lambda t,y:t-y,[34,76,90])
+# #     print(er)
+# # #     return er
+# # # minus ()
+
+# y="consultadd training"
+
+# x=[i for i in y if i not in 'aeiou']
+# print ("without vowels this string is: ", "".join(x))
+
+# e=[r for r in y if r in 'aeiou']
+# d={"".join(e)}
+# print("Vowels in this string are: ",d)
+
+# def add():
+#     a,b=10,20
+#     print (a+b)
+
+# add()
+
+# def check(t):
+#     d={}
+#     for i in t:
+#         if i.isdigit():
+#             if i in d:
+#                 d[i]+=1
+#             else:
+#                 d[i]=1
+#     return d
+
+# print (check("janki12234"))
+
+a,b =[1,2,3],[4,5,6]
+print (a+b)
+d=map(lambda a:a+=1, a+b)
+list(d)
