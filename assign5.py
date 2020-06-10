9f5894c1b9d4fd7665c51cb373eb3fc2aa3d4500
Janki Dhanani <jankidhanani@jankis-air.myfiosgateway.com>
2020-06-10 17:00:55 -0400

    assignment 5 and 6

---
+++ assign5.py
@@ -0,0 +1,21 @@
+# print("Ans. 1")
+
+# while True:
+#     try:
+#         a=int(input("Enter any value"))
+#         b=int(input("Enter any value"))
+#         res = a+b 
+#         print("addition is = ", res)
+#         break
+#     except:
+#         print("please enter only positive integer value")
+
+# print("Ans. 6")
+
+# def rdeven():
+#     rd=open("data.txt","r")
+#     e = rd.read()
+#     return (e[1:151:2])
+
+# print(rdeven())
+# print(len(rdeven()))
---
+++ assign6.py
@@ -0,0 +1,14 @@
+# print("Ans. 1")
+
+# o= range(1,100)
+# e=filter (lambda x:x%3!=0 and x%7==0, o)
+# print(list(e))
+
+# print("Ans. 2")
+# li=[1,2,3,4,5,6,7,8,9]
+
+# def multi(li):  
+#     ev = map(lambda x:x*x, li)
+#     return list(ev)
+
+# print(multi(li))
